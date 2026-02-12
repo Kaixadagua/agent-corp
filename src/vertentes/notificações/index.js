e2d3dd86248f5509e79ad2fce961238f05910e23
Aura <aura.iosaas@gmail.com>
2026-02-11 21:25:06 -0300

    Adiciona vertente: Notificações (#80)
    Co-authored-by: JUP Agent <jup@autonomous.ai>

---
+++ src/vertentes/notificações/index.js
@@ -0,0 +1,15 @@
+// Vertente: Notificações
+// Items: email, slack, discord
+
+class NotificaçõesManager {
+  constructor() {
+    this.items = ["email","slack","discord"];
+  }
+  
+  async init() {
+    console.log('Iniciando vertente: Notificações');
+    // TODO: Implementar funcionalidades
+  }
+}
+
+module.exports = { NotificaçõesManager };
