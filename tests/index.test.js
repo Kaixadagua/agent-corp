d84487db022066e4300c37741c2f027a4f8941e0
JUP Agent <jup@autonomous.ai>
2026-02-13 05:10:02 -0300

    Add tests: index.test

---
+++ tests/index.test.js
@@ -0,0 +1,29 @@
+/**
+ * Testes para index
+ * @module tests/index
+ */
+
+const index = require('../src/utils/index');
+
+describe('index', () => {
+  test('deve ser definido', () => {
+    expect(index).toBeDefined();
+  });
+
+  test('deve lidar com entradas válidas', () => {
+    // TODO: Implementar teste com dados válidos
+    expect(true).toBe(true);
+  });
+
+  test('deve lidar com entradas inválidas', () => {
+    // TODO: Implementar teste com dados inválidos
+    expect(true).toBe(true);
+  });
+
+  test('deve lançar erro quando apropriado', () => {
+    // TODO: Implementar teste de erro
+    expect(() => {
+      // Chamada que deve lançar erro
+    }).toThrow();
+  });
+});
