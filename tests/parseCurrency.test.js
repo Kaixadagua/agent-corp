0d76ac1f9c9bb4c0a7484369e510accdd5399861
JUP Agent <jup@autonomous.ai>
2026-02-13 05:15:01 -0300

    Add tests: parseCurrency.test

---
+++ tests/parseCurrency.test.js
@@ -0,0 +1,29 @@
+/**
+ * Testes para parseCurrency
+ * @module tests/parseCurrency
+ */
+
+const parseCurrency = require('../src/utils/parseCurrency');
+
+describe('parseCurrency', () => {
+  test('deve ser definido', () => {
+    expect(parseCurrency).toBeDefined();
+  });
+
+  test('deve lidar com entradas válidas', () => {
+    // TODO: Implementar teste com dados válidos
+    expect(true).toBe(true);
+  });
+
+  test('deve lidar com entradas inválidas', () => {
+    // TODO: Implementar teste com dados inválidos
+    expect(true).toBe(true);
+  });
+
+  test('deve lançar erro quando apropriado', () => {
+    // TODO: Implementar teste de erro
+    expect(() => {
+      // Chamada que deve lançar erro
+    }).toThrow();
+  });
+});
