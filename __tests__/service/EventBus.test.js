cc555fe14aecceb713dae063d1aec88e43e85902
JUP Agent <jup@autonomous.ai>
2026-02-16 09:31:02 -0300

    feat: Auto-commit 1 file(s) - Quality 38/50

---
+++ __tests__/service/EventBus.test.js
@@ -0,0 +1,21 @@
+const EventBus = require('../../src/services/EventBus');
+
+describe('EventBus Service', () => {
+  let service;
+
+  beforeEach(() => {
+    service = new EventBus();
+  });
+
+  test('should create item', () => {
+    const item = { id: 1, name: 'Test' };
+    const result = service.create(item);
+    expect(result).toEqual(item);
+  });
+
+  test('should find all items', () => {
+    const items = [{ id: 1 }, { id: 2 }];
+    items.forEach(item => service.create(item));
+    expect(service.findAll()).toHaveLength(2);
+  });
+});
