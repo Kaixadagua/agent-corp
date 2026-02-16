2ff4867be2d8b0cde4725e2d27d7b31eed929f30
JUP Agent <jup@autonomous.ai>
2026-02-16 09:57:02 -0300

    feat: Auto-commit 1 file(s) - Quality 38/50

---
+++ src/services/WsServer.js
@@ -0,0 +1,48 @@
+/**
+ * WsServer Service
+ * Gerencia operações de dados e regras de negócio
+ * @module services/WsServer
+ */
+
+/**
+ * Serviço para gerenciamento de WsServer
+ */
+class WsServer {
+  /**
+   * Cria instância do serviço
+   */
+  constructor() {
+    this.data = [];
+    this.nextId = 1;
+  }
+  
+  /**
+   * Cria novo item
+   * @param {Object} item - Dados do item a criar
+   * @returns {Object} Item criado com ID
+   */
+  create(item) {
+    const newItem = { ...item, id: this.nextId++, createdAt: new Date() };
+    this.data.push(newItem);
+    return newItem;
+  }
+  
+  /**
+   * Retorna todos os itens
+   * @returns {Array} Lista de todos os itens
+   */
+  findAll() {
+    return [...this.data];
+  }
+  
+  /**
+   * Busca item por ID
+   * @param {number} id - ID do item
+   * @returns {Object|undefined} Item encontrado
+   */
+  findById(id) {
+    return this.data.find(item => item.id === id);
+  }
+}
+
+module.exports = WsServer;
